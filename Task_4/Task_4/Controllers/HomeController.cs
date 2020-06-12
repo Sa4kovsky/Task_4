1e832075fadca67e1e6236e6903e86309dfbcfc4
Evgeny Sachkovsky <sa4kovsky@gmail.com>
2020-06-12 16:39:00 +0300

    create task

---
+++ Task_4/Task_4/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Task_4.Models;
+
+namespace Task_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "Unblock")]
+        public async Task<IActionResult> Index()
+        {
+            await _roleManager.CreateAsync(new IdentityRole { Name = "Unblock", NormalizedName = "Unblock" });
+            await _roleManager.CreateAsync(new IdentityRole { Name = "Block", NormalizedName = "Block" });
+            return View(await _roleManager.Roles.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
